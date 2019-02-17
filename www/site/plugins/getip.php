764f56dcacf03fe621f5bff98fb6381283a4ff04
Tristan Bagot <tristan.bagot@gmail.com>
2019-02-17 18:24:37 +0100

    default

---
+++ www/site/plugins/getip.php
@@ -0,0 +1,15 @@
+<?php
+
+function getUserIP() {
+    if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+            $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim($addr[0]);
+        } else {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
--- www/site/templates/default.php
+++ www/site/templates/default.php
@@ -1,1 +1,11 @@
-<?php go($site->homePage()) ?>
+<?php snippet('header') ?>
+
+<div id="menu">
+	<a href="<?= $site->url() ?>">Back</a>
+</div>
+
+<div id="collection">
+	<?= $page->text()->kt() ?>
+</div>
+
+<?php snippet('footer') ?>
